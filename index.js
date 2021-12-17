947e6926392ecb6ba0383c4916fb6dbb88a34784
Rafaela Dias <93568221+rafaeladns@users.noreply.github.com>
2021-12-16 23:48:28 -0300

    Iframe atualizado

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 
-// let botao = $('#pesquisar')
-// botao.on('click', function(){
-// $.ajax({
-//     url:`https://api.nasa.gov/planetary/apod?api_key=c0rZBhWA17eRlEVJekMQW8fFD2hAkMp4U9HaRH74&date=${date.value}`,
-//     success: function(result){
-//         $('#texto').html(`${result.explanation}`)
-//         $('#autor').html(`Autor:${result.copyright}`)
-//     $('#minhaImagem').attr('src', (result.url))
-//         console.log(result)
-//     }
-//     })
-// });
-
-
 let diaHoje = new Date();
 let dia = diaHoje.getDate();
 let mes = diaHoje.getMonth() + 1;
@@ -36,7 +22,15 @@ function requisicao(){
         success: function(result){
             $('#texto').html(`${result.explanation}`)
             $('#autor').html(`Autor:${result.copyright}`)
-        $('#minhaImagem').attr('src', (result.url))
+            if(result.media_type == 'image'){
+                $('#minhaImagem').attr('src', (result.url))
+                document.getElementById('minhaImagem').style.display = 'block'
+                document.getElementById('video').style.display = 'none'
+            }else{
+                document.getElementById('minhaImagem').style.display = 'none'
+                document.getElementById('video').style.display = 'block'
+                $('#video').attr('src', (result.url))
+            }
             console.log(result)
         }
         })
